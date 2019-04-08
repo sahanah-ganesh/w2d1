c22033867757c5dc94fbcf7f7c8036bd28811f36
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-04-08 16:28:55 +0000

    step 5 fix

--- http-functions.js
+++ http-functions.js
@@ -24,6 +24,10 @@ module.exports = function getHTML (options, callback) {
 
 }
 
+function printHTML (html) {
+  console.log(html);
+}
+
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step5.html'
