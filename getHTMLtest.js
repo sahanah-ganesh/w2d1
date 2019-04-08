dfc4c6ceb406ef0c8752c372f9bad333c1f14b80
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-04-08 16:09:24 +0000

    step 5 complete

---
+++ getHTMLtest.js
@@ -0,0 +1,12 @@
+var getHTML = require('./http-functions.js');
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+console.log(getHTML());
