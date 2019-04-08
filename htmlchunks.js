36bc16875fb03c0e4d4e359dfaa5378c73de462a
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-04-08 15:14:07 +0000

    completed chunks

---
+++ htmlchunks.js
@@ -0,0 +1,25 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      console.log('Chunk received. Length: ', data.length, '\n');
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+  });
+
+}
+console.log(getAndPrintHTMLChunks());
