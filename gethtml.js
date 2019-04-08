12d865cb2fac07e34567f2f177da12f92fb83ee5
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-04-08 15:55:32 +0000

    step 4 complete

---
+++ gethtml.js
@@ -0,0 +1,36 @@
+var https = require('https');
+
+function getHTML (options, callback) {
+
+  var output;
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+
+      output += data, '\n';
+
+      console.log('Chunks received:', output, '\n');
+    });
+
+    response.on('end', function() {
+
+      console.log('Response stream complete.');
+    });
+
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+console.log(getHTML());
