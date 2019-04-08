4ded2ee21a7d4261c82945f35107881949096310
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-04-08 15:29:56 +0000

    completed step2

---
+++ getprint.js
@@ -0,0 +1,27 @@
+var https = require('https');
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  var output = [];
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      output.push(data);
+      console.log('Chunks received:', data, '\n');
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+  });
+}
+console.log(getAndPrintHTML());
